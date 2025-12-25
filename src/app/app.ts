1ee06d8f1a9ae70e7abea97db63ceb69174fd0b5
Alexandre Zua Caldeira <zuacaldeira@gmail.com>
2025-12-25 01:39:01 +0100

    Defined app pages to have the structure HEADER ... FOOTER

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./common/header/header";
+import { Footer } from "./common/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
